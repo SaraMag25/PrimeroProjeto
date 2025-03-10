ab6ddabf17d6955c65584e028ab000bab6678e32
Sara <saramagalhaes242@gmail.com>
2025-03-10 14:51:46 -0300

    hello world

---
+++ PrimeroProjeto/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PrimeroProjeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
